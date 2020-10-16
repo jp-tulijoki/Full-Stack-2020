19aaddf02126c35c2745d44338b6bd81060ee4cd
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-10-16 18:34:03 +0300

    unicafen viilausta

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -24,12 +24,12 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <table>
       <tbody>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={all} />
-      <StatisticsLine text="average" value={average} />
-      <StatisticsLine text="positive" value={positive} />
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={all} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="positive" value={positive} />
       </tbody>  
     </table>
   )
