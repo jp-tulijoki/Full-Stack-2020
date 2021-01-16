2e5e0e534d2051e818cfad09e11295bee515ca5c
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-01-16 11:41:31 +0200

    4.2 valmis

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+let PORT = config.PORT
+
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
